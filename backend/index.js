8d3468e30d0152f16d425d1138d256d225ff6478
Greeshma2005 <greeshmasree02@gmail.com>
2025-09-04 22:36:33 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
